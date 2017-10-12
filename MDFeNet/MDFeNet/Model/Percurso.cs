9f84fa8d97de06c0182ce65c0c4f5b44f70875ff
Marco Polo Viana <marcopoloviana@hotmail.com>
2017-10-12 17:27:57 -0300

    Classe de UFs do percurso

---
+++ MDFeNet/MDFeNet/Model/Percurso.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MDFeNet.Model
+{
+    public class Percurso
+    {
+        public string ufPer { get; set; }
+
+        public string GetStatement()
+        {
+            return null;
+        }
+    }
+}
